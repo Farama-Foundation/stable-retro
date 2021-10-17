ebfec6a8c177d6307c8235aaa26568c84f9c9829
Mathieu Poliquin <mathieu.poliquin@gmail.com>
2021-10-17 21:41:02 +0800

    Updated NHL941on1 with custom script for reward function

---
+++ retro/data/stable/NHL941on1-Genesis/script.lua
@@ -0,0 +1,24 @@
+prev_p1_score = 0
+prev_p2_score = 0
+prev_p1_faceoffwon = 0
+prev_p1_shots = 0
+
+
+
+function nhl94_reward ()
+    local p1_score_reward =  (data.p1_score - prev_p1_score) * 1
+    local p2_score_reward =  (data.p2_score - prev_p2_score) * -1
+    local p1_faceoffwon_reward =  (data.p1_faceoffwon - prev_p1_faceoffwon) * 1
+
+    local p1_shots_reward = 0
+    if data.p1_y >= 150 then
+        p1_shots_reward = (data.p1_shots - prev_p1_shots) * 1
+    end
+
+    prev_p1_score = data.p1_score
+    prev_p2_score = data.p2_score
+    prev_p1_faceoffwon = data.p1_faceoffwon
+    prev_p1_shots = data.p1_shots
+
+    return p1_score_reward + p2_score_reward + p1_faceoffwon_reward + p1_shots_reward
+end

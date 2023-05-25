21451dada65d0bee2f3551803847d179f50229e7
Tyson <tyson@onaclovtech.com>
2023-05-25 07:18:23 -0600

    Fixed windows batch build file

--- docker/build_windows.bat
+++ docker/build_windows.bat
@@ -1,6 +1,7 @@
 cd ..
-docker build . --tag farama-foundation/stable-retro:win64-v2 --file docker/windows/Dockerfile.build --progress plain \
-&& docker run -i -t farama-foundation/stable-retro:win64-v2 /bin/bash
+docker build . --tag farama-foundation/stable-retro:win64-v2 --file docker/windows/Dockerfile.build --progress plain
+
+docker run -i -t farama-foundation/stable-retro:win64-v2 /bin/bash
 
 REM Example of how you would extract the data
 REM docker cp 915ff70ebc56:/root/stable-retro/dist/gym_retro-0.9.0-cp310-cp310-win_amd64.whl .

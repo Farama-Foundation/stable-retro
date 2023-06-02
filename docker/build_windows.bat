d990b1892a07bc2d821934fb5202a29efa3a7706
Tyson Bailey <onaclov2000+github@gmail.com>
2023-06-02 06:58:41 -0600

    Update build_windows.bat

--- docker/build_windows.bat
+++ docker/build_windows.bat
@@ -6,3 +6,4 @@ docker run -i -t farama-foundation/stable-retro:win64-v2 /bin/bash
 REM Example of how you would extract the data
 REM docker cp 915ff70ebc56:/root/stable-retro/dist/gym_retro-0.9.0-cp310-cp310-win_amd64.whl .
 REM pip install retro/dist/gym_retro-0.9.0-cp310-cp310-win_amd64.whl
+

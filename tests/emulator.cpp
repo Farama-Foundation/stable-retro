df342c8b066594231de1a0515317d81090e9db51
Mathieu Poliquin <mathieu.poliquin@gmail.com>
2023-06-20 14:07:03 +0800

    Fix emulator tests - missing 32x entry

--- tests/emulator.cpp
+++ tests/emulator.cpp
@@ -27,7 +27,7 @@ struct EmulatorTestParamName {
 };
 
 void EmulatorTest::SetUp() {
-	for (const string& core : { "fceumm", "gambatte", "genesis_plus_gx", "mednafen_pce_fast", "mgba", "snes9x", "stella" }) {
+	for (const string& core : { "fceumm", "gambatte", "genesis_plus_gx", "mednafen_pce_fast", "mgba", "snes9x", "stella", "picodrive" }) {
 		ifstream in("../retro/cores/" + core + ".json");
 		ostringstream out;
 		Retro::corePath("../retro/cores");

f7f7c9dfd134896f20626557334b614023bc78c1
zbeucler2018 <49871927+zbeucler2018@users.noreply.github.com>
2023-07-15 07:12:42 -0400

    Fix installation with git via pip (#37)

--- retro/__init__.py
+++ retro/__init__.py
@@ -8,7 +8,7 @@ from retro.retro_env import RetroEnv
 ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
 core_path(os.path.join(os.path.dirname(__file__), "cores"))
 
-with open(os.path.join(os.path.dirname(__file__), "../VERSION")) as f:
+with open(os.path.join(os.path.dirname(__file__), "VERSION")) as f:
     __version__ = f.read()
 
 
